9e0c575ab1427f7ebe1dddf246d3a1ee43ff66df
Jason Carcamo <jasoncarcamo30@yahoo.com>
2019-09-20 02:23:09 -0400

    update

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+
 module.exports = {
     PORT: process.env.PORT || 8000,
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://jason:carcamo11@localhost/shoe-comp-db'
