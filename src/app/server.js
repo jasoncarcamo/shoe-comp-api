0352037e52a8233da2386eae1e1cc83de144a542
Jason Carcamo <jasoncarcamo30@yahoo.com>
2019-09-20 00:54:04 -0400

    update

--- src/app/server.js
+++ src/app/server.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
-const {PORT, DATABASE_URL} = require("../../config");
+const {PORT} = require("../../config");
 const app = require("./app");
 const knex = require("knex");
 
 const db = knex({
     client: 'pg',
-    connection: DATABASE_URL
+    connection: process.env.DATABASE_URL
 })
 
 app.set("db", db);
