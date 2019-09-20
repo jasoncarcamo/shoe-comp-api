aaed926c1fabcb691c0e6577f52ea0c73112a598
Jason Carcamo <jasoncarcamo30@yahoo.com>
2019-09-20 02:17:46 -0400

    update

--- src/app/app.js
+++ src/app/app.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
-const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 
-app.use(morgan("tiny"))
 app.use(cors());
 app.use(helmet());
 
