4667c075945a1e40e9b161fd8434a8b20764f269
Jason Carcamo <jasoncarcamo30@yahoo.com>
2019-09-20 23:34:22 -0400

    update

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 
 module.exports = {
     PORT: process.env.PORT || 8000,
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://jason:carcamo11@localhost/shoe-comp-db'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://jason:carcamo11@localhost/shoe-comp-db',
+    NODE_ENV: process.env.NODE_ENV || 'development'
 }
